b8ca655aaba8a996893e2d3aa265531fe2405ddc
ddssii123 <40882736+ddssii123@users.noreply.github.com>
2018-09-06 19:20:20 +0900

    Add files via upload

--- YNL/bootstrap/join/join.php
+++ YNL/bootstrap/join/join.php
@@ -1,6 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+$is_logged = $_SESSION['is_logged'];
+if($is_logged=='YES') {
+    $user_id = $_SESSION['user_id'];
+    $message = $user_id . ' 님, 로그인 했습니다.';
+}
+else {
+    $message = '로그인이 실패했습니다.';
+}
 
+?>
 <head>
   <title>You not Lonely</title>
   <meta charset="UTF-8">
