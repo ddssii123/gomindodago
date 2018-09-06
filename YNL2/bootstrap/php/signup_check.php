99c77783603078fd3b7fb59bda918937563895a8
ddssii123 <40882736+ddssii123@users.noreply.github.com>
2018-09-06 20:21:21 +0900

    Add files via upload

---
+++ YNL2/bootstrap/php/signup_check.php
@@ -0,0 +1,20 @@
+<?php
+include_once ('../config.php');
+$mysqli = new mysqli($DB['host'], $DB['id'], $DB['pw'], $DB['db']);
+if (mysqli_connect_error()) {
+    exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+}
+
+extract($_POST);
+
+echo $user_id. '<br />';
+echo $user_pass. '<br />';
+echo $user_pass2. '<br />';
+echo $user_email. '<br />';
+
+$q = "INSERT INTO ap_member ( id, pw, email ) VALUES ( '$user_id', '$user_pass', '$user_email' )";
+
+$mysqli->query( $q);
+
+$mysqli->close();
+?>
